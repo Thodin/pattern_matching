3ac5a5a6677bc04286302c99e340e6368375948c
Max Meindl <max_meindl@gmx.de>
2025-01-31 20:01:52 +0100

    Removed everything except minimal implementation.

--- src/geomtery/shape_operations.rs
+++ src/geomtery/shape_operations.rs
@@ -2,10 +2,21 @@ use std::f64::consts::PI;
 
 use super::shapes::Shape;
 
-pub fn calc_area(shape: &Shape) -> f64 {
+pub fn calc_area(shape: &Shape) {
     match shape {
-        Shape::Circle(radius) => radius.powi(2) * PI,
-        Shape::Square(side) => side.powi(2),
-        Shape::Rectangle(width, height) => width * height,
+        Shape::Circle(radius) => println!(
+            "Area of circle with radius {:.4}: {:.4}",
+            radius,
+            radius.powi(2) * PI
+        ),
+        Shape::Square(side) => {
+            println!("Area of square with side {:.4}: {:.4}", side, side.powi(2))
+        }
+        Shape::Rectangle(width, height) => println!(
+            "Area of rectangle with width {:.4} and height {:.4}: {:.4}",
+            width,
+            height,
+            width * height
+        ),
     }
 }
